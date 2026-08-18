a16ebf475a0d82da3b4d7857be78070d1fb47fbd
jagadiswarambati <srijagadishreddy@gmail.com>
2026-08-18 06:21:50 +0530

    support deployment port

--- backend/app.py
+++ backend/app.py
@@ -932,7 +932,6 @@ if __name__ == "__main__":
 
 
     app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=False
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000))
     )
